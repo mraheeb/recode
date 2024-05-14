9cb4526107188c80792e7885c04c467c6ab1935e
sinan <sinan.mohd593@gmail.com>
2024-05-14 22:08:52 +0530

    testing phase

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ def generate():
 
 @app.route("/api/generateProjectDocumentation", methods=["POST"])
 def generateProjectDocumentation():
-    print("/generateProjectDocumentation")
+    print("------------------------------------------------------------------------------")
 
     if not os.path.exists("uploads"):
         os.makedirs("uploads")
@@ -33,19 +33,84 @@ def generateProjectDocumentation():
         return "Failed to upload and modify file"
 
 
-@app.route("/api/generateInlineDocumentation", methods=["POST"])
-def generateInlineDocumentation():
-    print("/generateInlineDocumentation")
+@app.route("/api/convertCode", methods=["POST"])
+def convertCode():
+    print("------------------------------------------------------------------------------")
     json_data = request.json
     query = json_data.get("query")
     current_language = json_data.get("current_language")
     target_language = json_data.get("target_language")
 
-    response = utils.translate(query, current_language, target_language, llm)
+    isCodeValid = utils.validate(query, current_language, llm)
+    if "Yes" in isCodeValid:
+        response = utils.translate(query, current_language, target_language, llm)
+    else:
+        response = {"error": f"The code given cannot be converted as it is not a valid {current_language} code."}
+        return response
+    
+    # documented_response = utils.document(query, target_language, llm)
+
+    # print(documented_response.get("result"))
+    # print("------------------------------------------------------------------------------")
+    # return documented_response
+    
+    python_code = response.get("result", "")
+    start_index = python_code.find("$start$") + len("$start$")
+    stop_index = python_code.find("$end$") + len("$end$")
+
+    response = python_code[start_index:stop_index - 5]
+
+    response = {"result": response}
+
+    print(response.get("result"))
+
+    print("------------------------------------------------------------------------------")
+    return response
+
+@app.route("/api/generateLegacyDocumentation", methods=["POST"])
+def generateLegacyDocumentation():
+    print("------------------------------------------------------------------------------")
+    json_data = request.json
+    query = json_data.get("query")
+    current_language = json_data.get("current_language")
+
+    isCodeValid = utils.validate(query, current_language, llm)
+    if "Yes" in isCodeValid:
+        response = utils.document(query, current_language, llm)
+    else:
+        response = {"error": f"The code given cannot be documented as it is not a valid {current_language} code."}
+        return response
+
+
+    documentation = response.get("result", "")
+    start_index = documentation.find("$start$") + len("$start$")
+    stop_index = documentation.find("$end$") + len("$end$")
+
+    documentation = documentation[start_index:stop_index - 5]
+
+    final_documentation = documentation + "\n\n\n" + query
+
+    response = {
+        "result": final_documentation
+    }
+
+    # response = documentation[start_index:stop_index - 5]
+
+    # response = {"result": response}
+    
+    print(response.get("result"))
+
+    print("------------------------------------------------------------------------------")
+    return response
+
+@app.route("/api/validate", methods=["POST"])
+def validate():
+    json_data = request.json
+    query = json_data.get("query")
+    current_language = json_data.get("current_language")
+    response = utils.validate(query, current_language, llm)
 
     return response
-    #return "Working"
-
 
 def start_app():
     print("This function gets called!")
--- utils/utils.py
+++ utils/utils.py
@@ -6,9 +6,6 @@ def uploadFileToServer(request):
     if 'file' not in request.files:
         return "No file part"
     
-    # if 'prompt_data' not in request.files:
-    #     return "No Prompt Data"
-    
     file = request.files['file']
 
     if file.filename == '':
@@ -27,31 +24,75 @@ def uploadFileToServer(request):
     return None
 
 def translate(query, current_language, target_language, llm):
-    # Placeholder for your code variable
     code = "{" + query + "}"
 
-    # Prompt for the LLM
+#     prompt = f"""
+# {code}
+# for the given code do as per the given following constraints and respond nothing else other than what i say in the constraints
+# note:Don't respond with anything other than the {target_language} code with proper documentation
+# 1. Check whether the given code is a {current_language} language code or not. If yes, then continue with other constraints and don't respond with anything else other than the generated {target_language} code and the documentation for the code after the code is completely generated and no other words from you. Else stop executing and respond "invalid {current_language} code" and nothing else.
+# 2. The provided {current_language} code snippet must adhere to standard {current_language} syntax and conventions. It should be a syntactically correct {current_language} program.
+# 3. The {target_language} conversion should accurately reflect the functionality of the original {current_language} code. This includes preserving the logic, structure, and behavior of the {current_language} program.
+# 4. Proper documentation, including comments, should be included in the translated {target_language} code. The comments should explain the logic and purpose of the code, as well as any significant differences between the {current_language} and {target_language} versions.
+# 5. The program should handle errors gracefully. If the provided {current_language} code is not valid or encounters issues during conversion, the program should indicate the problem(s) encountered and provide meaningful error messages.
+#     """
+
+#     prompt = f"""
+# {code}
+# Identify the language the program is written in; if not a programming language return response as "NOT A PROGRAMMING LANGUAGE".
+# After Identifying the language, accurately predict what the program does, dont output it out yet.
+# Convert the input code into equivalent python code, if there are missing code snippets like external library/modules mention it using python comments.
+# The previous prediction also needs to be commented inline where appropriate.
+
+# Always enclose the executable python code within $start$ and $end$.
+#     """
+
     prompt = f"""
 {code}
-for the given code do as per the given following constraints and respond nothing else other than what i say in the constraints
-note:Don't respond with anything other than the {target_language} code with proper documentation
-1. Check whether the given code is a {current_language} language code or not. If yes, then continue with other constraints and don't respond with anything else other than the generated {target_language} code and the documentation for the code after the code is completely generated and no other words from you. Else stop executing and respond "invalid {current_language} code" and nothing else.
-2. The provided {current_language} code snippet must adhere to standard {current_language} syntax and conventions. It should be a syntactically correct {current_language} program.
-3. The {target_language} conversion should accurately reflect the functionality of the original {current_language} code. This includes preserving the logic, structure, and behavior of the {current_language} program.
-4. Proper documentation, including comments, should be included in the translated {target_language} code. The comments should explain the logic and purpose of the code, as well as any significant differences between the {current_language} and {target_language} versions.
-5. The program should handle errors gracefully. If the provided {current_language} code is not valid or encounters issues during conversion, the program should indicate the problem(s) encountered and provide meaningful error messages.
+Identify the language the program is written in; if not a programming language return response as "NOT A PROGRAMMING LANGUAGE".
+After Identifying the language, accurately predict what the program does, dont output it out yet.
+Convert the input code into equivalent {target_language} code, if there are missing code snippets like external library/modules mention it using {target_language} comments.
+The previous prediction also needs to be commented inline where appropriate.
+
+Always enclose the executable {target_language} code within $start$ and $end$.
     """
 
-    # Print prompt for debugging
-    print(prompt)
 
-    # Invoke LLN (Large Language Model)
     query_response = llm.invoke(prompt)
 
-    # Print LLN response for debugging
-    print(query_response)
 
-    # Prepare response dictionary
     response = {"result": query_response}
 
     return response
+
+def document(query, current_language, llm):
+    code = "{" + query + "}"
+
+    prompt = f"""
+{code}
+ Generate documentation for the code and respond back always by enclosing the documentation  $start$ and $end$
+    """
+
+
+    query_response = llm.invoke(prompt)
+
+
+    response = {"result": query_response}
+
+    return response
+
+def validate(query, current_language, llm):
+    code = "{" + query + "}"
+
+    prompt = f"""
+{code}
+Respond back with a one word(Yes/No). Is the given code {code} written in {current_language}
+    """
+
+
+    query_response = llm.invoke(prompt)
+
+
+    response = {"result": query_response}
+
+    return response.get("result")
